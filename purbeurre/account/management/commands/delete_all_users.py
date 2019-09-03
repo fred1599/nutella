f41fa166d70148188ac31d0d5df6f8df2076cfea
frederic.juppont <frederic.juppont@gmail.com>
2019-09-03 23:09:55 +0200

    modif

--- purbeurre/account/management/commands/delete_all_users.py
+++ purbeurre/account/management/commands/delete_all_users.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-from purbeurre.purebeurre.settings import config
+from purbeurre.settings import config
 
 
 class Command(BaseCommand):
