0c19d88ca209f6bce8a1d4e89eefff33bb257f3d
frederic.juppont <frederic.juppont@gmail.com>
2019-09-04 15:35:25 +0200

    port email

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -93,6 +93,7 @@ TEMPLATES = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('HOST')
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
 EMAIL_PORT = os.environ.get('PORT')
 EMAIL_HOST_USER = os.environ.get('ACCOUNT')
 EMAIL_HOST_PASSWORD = os.environ.get('PASSWORD')
