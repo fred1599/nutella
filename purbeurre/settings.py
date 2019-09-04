1b02f8443fca21113476d7bdb7f18f25579c7631
frederic.juppont <frederic.juppont@gmail.com>
2019-09-04 11:28:16 +0200

    debug False

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -17,15 +17,18 @@ from configparser import RawConfigParser
 import dj_database_url
 import sys
 
+from configparser import RawConfigParser
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = [
     '*',
