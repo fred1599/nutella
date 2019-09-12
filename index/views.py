12c5cdddb437fc1aa5ac5bec3740d4831aaff616
frederic.juppont <frederic.juppont@gmail.com>
2019-09-12 07:59:49 +0200

    ajout page mentions

--- index/views.py
+++ index/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):
     user = request.user
     return render(request, 'index/accueil.html', {'user': user})
+
+
+def mentions(request):
+    return render(request, 'index/mentions.html', )
